7ffd153583e6b337c963bb81f860f4ab708e6fcb
unknown <xfvjykk@yandex.ru>
2022-12-26 20:42:41 +0300

    Сделал задание

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число M:");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+
+int CalculateSumm(int M, int N)
+{
+int start = M;
+int end = N;
+if(M > N)
+{
+start = N;
+end = M;
+}
+return (end + start)*(end - start + 1)/2;
+}
+
+Console.WriteLine(CalculateSumm(M, N));
+	
